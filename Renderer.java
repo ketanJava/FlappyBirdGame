0c6c6de5824372e853e9f58bae99c8e895aca852
Ketan <k6890920@gmail.com>
2021-06-10 13:35:29 -0400

    Updated Renderer

--- Renderer.java
+++ Renderer.java
@@ -3,9 +3,10 @@ package flappyBird;
 import javax.swing.*;
 import java.awt.*;
 
+//Renderer class to inherit the paint properties of JPanel class and perform the paint job.
 public class Renderer extends JPanel {
     @Override
-    protected void paintComponent(Graphics g){
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         FlappyBird.flappyBird.repaint(g);
     }
